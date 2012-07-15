bb2a122d628932b6f13d817a7e2c8cc8cd5072cf
Danny Su <danny@well.ca>
2012-07-15 14:16:54 -0400

    Initial mimic of wordpress url style

--- site.hs
+++ site.hs
@@ -3,8 +3,17 @@ import Control.Arrow ((>>>))
 
 import Hakyll
 
+{- Custom deploy command -}
+hakyllConf = defaultHakyllConfiguration {
+        deployCommand = "rsync -avz --delete _site/ thedumbkid@dannysu.com:~/dannysu/hakyll"
+    }
+
+{- Main program -}
 main :: IO ()
-main = hakyll $ do
+main = hakyllWith hakyllConf $ do
+
+    match "templates/*" $ compile templateCompiler
+
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
@@ -13,7 +22,13 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match "templates/*" $ compile templateCompiler
+    {- Match all files under posts directory and its subdirectories. -}
+    {- Turn posts into wordpress style url: year/month/date/title/index.html -}
+    match "posts/**" $ do
+        route   $ gsubRoute "posts/" (const "") `composeRoutes` gsubRoute ".markdown" (const "/index.html")
+        compile $ pageCompiler
+            >>> applyTemplateCompiler "templates/default.html"
+            >>> relativizeUrlsCompiler
 
     match (list ["about.rst", "index.markdown", "code.lhs"]) $ do
         route   $ setExtension "html"
